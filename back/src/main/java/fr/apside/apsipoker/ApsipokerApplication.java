254e3246abf1d5dba6d240ba1af8f20990eea877
Jérémy Simar <simar.jeremy@live.fr>
2020-11-06 08:24:43 +0100

    [BACK] feat: init

---
+++ back/src/main/java/fr/apside/apsipoker/ApsipokerApplication.java
@@ -0,0 +1,13 @@
+package fr.apside.apsipoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApsipokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApsipokerApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/fr/apside/apsipoker/ApsipokerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.apside.apsipoker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApsipokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
